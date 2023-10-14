015ba5bb046e4a65ebdc174ccd2edbcfd91354ee
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-14 20:05:26 +0500

    image optimization commit

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,12 +1,16 @@
 'use client'
 import { ProductType } from "@/interface";
 import Image from "next/image";
+import Link from "next/link";
 import { FC } from 'react'
+import CustomImage from "./images";
 const Product: FC<{ product: ProductType }> = ({ product }) => {
 
   return (
-    <div className="bg-gray-100 p-6 rounded-lg">
-      <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" />
+    <div className="  h-96 flex flex-col p-6 rounded-lg group hover:scale-105 transition-transform ease-out duration-200 border">
+      <Link href={`/product/${product.id}`} className="relative max-h-80 flex-1 ">
+        <CustomImage product={product} fill/>
+      </Link>
       <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{product.category}</h3>
       <div className="font-semibold flex items-center justify-between mt-4 mb-1">
       <p className="w-44 truncate">{product.title}</p>

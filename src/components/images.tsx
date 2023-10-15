077ddd43d25a67c25dbcd15d7d1bb94aa2e208c3
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-15 23:54:25 +0500

    product details

--- src/components/images.tsx
+++ src/components/images.tsx
@@ -9,18 +9,35 @@ interface Props {
 const CustomImage: FC<Props> = ({product, fill }) => {
     const [isLoading, setLoading] = useState(true)
     return (
-        <Image  
-        src={product.image}
-        alt={product.title}
-        fill
-        className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
-            isLoading
-            ?'scale-110 blur-2xl grayscale'
-            :'scale-100 blur-0 grayscale-0'
-        }}`}
-        onLoadingComplete={()=> setLoading(false)}
-        
-        />
+       <>
+       
+       {fill ? (
+				<Image
+					src={product.image}
+					alt={product.title}
+					fill
+					className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
+						isLoading
+							? 'scale-110 blur-2xl grayscale'
+							: 'scale-100 blur-0 grayscale-0'
+					}}`}
+					onLoadingComplete={() => setLoading(false)}
+				/>
+			) : (
+				<Image
+					src={product.image}
+					alt={product.title}
+					width={400}
+					height={1000}
+					className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
+						isLoading
+							? 'scale-110 blur-2xl grayscale'
+							: 'scale-100 blur-0 grayscale-0'
+					}}`}
+					onLoadingComplete={() => setLoading(false)}
+				/>
+			)}
+            </>
     );
 };
 export default CustomImage;
